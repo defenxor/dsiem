06e2c4dc52619c26ab2679a79e81fb94454d18bd
mmta <mmta.gm@gmail.com>
2018-09-02 20:54:30 +0700

    breakdown main

--- main.go
+++ main.go
@@ -4,14 +4,11 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
-	"os"
 	"strconv"
 	"sync/atomic"
 
 	"github.com/julienschmidt/httprouter"
-	"github.com/kardianos/osext"
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
 )
@@ -136,34 +133,6 @@ func main() {
 	router.POST("/*file", handle)
 	logger.Info("Server listening on port: ", port)
 	logger.Fatal(http.ListenAndServe(":"+port, router))
-}
-
-func setupLogger() {
-	formatter := &logrus.TextFormatter{
-		FullTimestamp: true,
-	}
-	// formatter := &logrus.JSONFormatter{}
-	logger.Formatter = formatter
-	logger.Out = os.Stdout
-
-	// use logrus for standard log output, those chatty 3rd-party libs ..
-	log.SetOutput(logger.Writer())
-	log.SetFlags(log.Flags() &^ (log.Ldate | log.Ltime))
-}
-
-func logAndExit(err error) {
-	// time.Sleep(5 * time.Minute)
-	logger.Fatalf("%+v", errors.Wrap(err, ""))
-}
-
-func getDir() (string, error) {
-	dir, err := osext.ExecutableFolder()
-	return dir, err
-}
-
-func fileExist(path string) bool {
-	_, err := os.Stat(path)
-	return !os.IsNotExist(err)
 }
 
 func verify(e normalizedEvent) error {
