11a5b682a37ef64c198fd045c8927a811a2f44aa
mmta <mmta.gm@gmail.com>
2018-10-24 00:49:06 +0700

    add elastic dep

---
+++ cmd/dpluger/plugin_template.go
@@ -0,0 +1,32 @@
+package main
+
+import "reflect"
+
+type pluginTemplate struct {
+	P          Plugin
+	Creator    string
+	CreateDate string
+	R          tsvRef
+}
+
+func (pt pluginTemplate) IsPluginRule() bool {
+	return pt.P.Type == "PluginRule"
+}
+
+func (pt pluginTemplate) IsFieldActive(name string) bool {
+	s := reflect.ValueOf(&pt.P.Fields).Elem()
+	typeOfT := s.Type()
+	for i := 0; i < s.NumField(); i++ {
+		f := s.Field(i)
+		if typeOfT.Field(i).Name == name {
+			v := f.Interface().(string)
+			return v != ""
+		}
+	}
+	return false
+}
+
+func (pt pluginTemplate) IsIntegerMutationRequired() bool {
+	return pt.IsFieldActive("PluginID") || pt.IsFieldActive("PluginSID") ||
+		pt.IsFieldActive("SrcPort") || pt.IsFieldActive("DstPort")
+}
