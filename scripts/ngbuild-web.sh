1f5525114ffdf8400094b026e032afadc3d28801
mmta <mmta.gm@gmail.com>
2018-11-09 13:13:50 +0700

    fix web build error

--- scripts/ngbuild-web.sh
+++ scripts/ngbuild-web.sh
@@ -3,6 +3,6 @@ dir="ui"
 [ "$1" == "internal" ] && dir="${dir}-internal"
 cd web/$dir
 npm install
-ng build --prod --build-optimizer --base-href /ui/
+npm run ng -- build --prod --build-optimizer --base-href /ui/
 rm -rf ../dist
 cp -r ./dist ../
