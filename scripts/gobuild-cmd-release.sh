e1d7827b4c56e5f6d31d9f1ad463f8db14bfbafa
mmta <mmta.gm@gmail.com>
2018-11-09 14:37:44 +0700

    default to draft release and more verb. deploy msg

--- scripts/gobuild-cmd-release.sh
+++ scripts/gobuild-cmd-release.sh
@@ -23,15 +23,19 @@ for os in $goos; do
     [ ! -d $c ] && echo $c directory doesnt exist, skipping. && continue
     n=$(basename $c)
     [ "$os" == "windows" ] && n="${n}.exe"
-    echo building $c ver=${ver} buildtime=${now} for $os ..
+    echo building $c ver=${ver} buildtime=${now} for $os: $n ..
     GOFLAGS="-mod=vendor" CGO_ENABLED=0 GOOS=$os GOARCH=amd64 go build -a -ldflags "-s -w -X main.version=${ver} -X main.buildTime=${now} -extldflags '-static'" -o $bdir/$n $c
   done
   mkdir -p $bdir/web/dist && cp -r ./web/dist/* $bdir/web/dist/ || exit 1
   cp -r ./configs ./LICENSE ./README.md $bdir/
-  cd $bdir 
-  zip -9 -r $rdir/dsiem-server_$os_amd64.zip dsiem configs web LICENSE README.md
+  cd $bdir
+  zname="$rdir/dsiem-server_${os}_amd64.zip"
+  echo "creating $zname .."
+  zip -9 -r $zname dsiem configs web LICENSE README.md
   tools=$(ls | grep -v dsiem | grep -v configs | grep -v web)
-  zip -9 $rdir/dsiem-tools_$os_amd64.zip $tools
+  zname="$rdir/dsiem-tools_${os}_amd64.zip"
+  echo "creating $zname .."
+  zip -9 $zname $tools
   cd $curdir
   rm -rf $bdir
 done
@@ -39,3 +43,5 @@ cd $rdir
 for f in $(ls $rdir | grep -v sha256); do
   sha256sum $f > $f.sha256
 done
+echo "Done building, content of $dir:"
+ls -l $rdir
