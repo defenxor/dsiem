5581a62ac9309e062a781a769d29e9339af57d38
mmta <mmta.gm@gmail.com>
2018-11-15 14:47:09 +0700

    add used elasticapm funcs to apm package

--- internal/pkg/shared/apm/apm.go
+++ internal/pkg/shared/apm/apm.go
@@ -18,15 +18,83 @@
 
 package apm
 
+import (
+	"sync"
+	"time"
+
+	"github.com/elastic/apm-agent-go"
+)
+
 var enabled bool
 var distributed bool
+var mu = sync.RWMutex{}
 
 //Enabled returns whether apm is enabled
 func Enabled() bool {
+	mu.RLock()
+	defer mu.RUnlock()
 	return enabled
 }
 
 //Enable set apm status
 func Enable(e bool) {
+	mu.Lock()
 	enabled = e
+	mu.Unlock()
+}
+
+// Transaction wraps transaction from elasticapm Default tracer and make it concurrency safe
+type Transaction struct {
+	sync.Mutex
+	Tx *elasticapm.Transaction
+}
+
+// StartTransaction returns a mutex protected elasticapm.Transaction, with optional starting time
+func StartTransaction(name, transactionType string, startTime *time.Time) *Transaction {
+	txObj := Transaction{}
+	if startTime != nil {
+		opts := elasticapm.TransactionOptions{TraceContext: elasticapm.TraceContext{}, Start: *startTime}
+		txObj.Tx = elasticapm.DefaultTracer.StartTransactionOptions(name, transactionType, opts)
+	} else {
+		txObj.Tx = elasticapm.DefaultTracer.StartTransaction(name, transactionType)
+	}
+	return &txObj
+}
+
+// Recover returns a elasticapm.DefaultTracer.Recover function to be deferred
+func (t *Transaction) Recover() func() {
+	f := func() {
+		elasticapm.DefaultTracer.Recover(t.Tx)
+	}
+	return f
+}
+
+// SetCustom set custom value for the transaction
+func (t *Transaction) SetCustom(key string, value interface{}) {
+	t.Lock()
+	t.Tx.Context.SetCustom(key, value)
+	t.Unlock()
+}
+
+// Result set the result for the transaction
+func (t *Transaction) Result(value string) {
+	t.Lock()
+	t.Tx.Result = value
+	t.Unlock()
+}
+
+// SetError set and send error fom the transaction
+func (t *Transaction) SetError(err error) {
+	t.Lock()
+	defer t.Unlock()
+	e := elasticapm.DefaultTracer.NewError(err)
+	e.Transaction = t.Tx
+	e.Send()
+}
+
+// End completes the transaction
+func (t *Transaction) End() {
+	t.Lock()
+	t.Tx.End()
+	t.Unlock()
 }
