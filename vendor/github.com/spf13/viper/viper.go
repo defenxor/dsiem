3d3d08352de3706490fea0579935c4e2f2155110
mmta <mmta.gm@gmail.com>
2018-11-17 03:26:15 +0700

    revert viper to v1.1.0

--- vendor/github.com/spf13/viper/viper.go
+++ vendor/github.com/spf13/viper/viper.go
@@ -306,9 +306,7 @@ func (v *Viper) WatchConfig() {
 							if err != nil {
 								log.Println("error:", err)
 							}
-							if v.onConfigChange != nil {
-								v.onConfigChange(event)
-							}
+							v.onConfigChange(event)
 						}
 					}
 				case err := <-watcher.Errors:
@@ -650,10 +648,8 @@ func (v *Viper) Get(key string) interface{} {
 			return cast.ToBool(val)
 		case string:
 			return cast.ToString(val)
-		case int32, int16, int8, int:
+		case int64, int32, int16, int8, int:
 			return cast.ToInt(val)
-		case int64:
-			return cast.ToInt64(val)
 		case float64, float32:
 			return cast.ToFloat64(val)
 		case time.Time:
@@ -1143,7 +1139,7 @@ func (v *Viper) SetDefault(key string, value interface{}) {
 	deepestMap[lastKey] = value
 }
 
-// Set sets the value for the key in the override register.
+// Set sets the value for the key in the override regiser.
 // Set is case-insensitive for a key.
 // Will be used instead of values obtained via
 // flags, config file, ENV, default, or key/value store.
