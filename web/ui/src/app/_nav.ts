897948dd4e4d7ab946c56c4e7570ea4aea00475f
Cempakers87 <iman.nistelrooy@gmail.com>
2018-11-22 16:00:53 +0700

    first commit

---
+++ web/ui/src/app/_nav.ts
@@ -0,0 +1,11 @@
+export const navItems = [
+  {
+    title: true,
+    name: 'Data'
+  },
+  {
+    name: 'Alarm List',
+    url: '/data/alarm-list',
+    icon: 'icon-list'
+  }
+];
---
+++ web/ui/src/app/app.routing.ts
@@ -0,0 +1,49 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+// Import Containers
+import { DefaultLayoutComponent } from './containers';
+
+import { P404Component } from './views/error/404.component';
+import { P500Component } from './views/error/500.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'data/alarm-list',
+    pathMatch: 'full',
+  },
+  {
+    path: '404',
+    component: P404Component,
+    data: {
+      title: 'Page 404'
+    }
+  },
+  {
+    path: '500',
+    component: P500Component,
+    data: {
+      title: 'Page 500'
+    }
+  },
+  {
+    path: '',
+    component: DefaultLayoutComponent,
+    data: {
+      title: 'Home'
+    },
+    children: [
+      {
+        path: 'data',
+        loadChildren: './views/base/base.module#BaseModule'
+      },
+    ]
+  }
+];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule {}
