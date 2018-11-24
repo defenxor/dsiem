40c7d1a267e61dc2f165386e9c4d65433b833e3d
mmta <memet.anwar@defenxor.com>
2018-11-25 01:03:11 +0700

    test: increase timeout for worker test [skip ci]

--- internal/pkg/dsiem/worker/worker_test.go
+++ internal/pkg/dsiem/worker/worker_test.go
@@ -140,7 +140,7 @@ func TestWorker(t *testing.T) {
 		err = Start(ch, msq, msqPrefix, nodeName, wd, frontend)
 		if err != nil {
 			fmt.Println("Flaky worker test result detected, attempted start so far:", i+1, "..")
-			time.Sleep(time.Second)
+			time.Sleep(time.Second * 2)
 		} else {
 			break
 		}
