34016920def7891b1e4750c31fb1d3e44c47e115
mmta <mmta@users.noreply.github.com>
2018-11-27 02:55:43 +0700

    fix: APM panic due to multiple calls to End() (#31)
    * refactor: use apm pkg in xcorrelator
    
    * refactor: fix typo
    
    * fix error in APM due to multiple calls to End()
    
    * fix unlock and cleanup comment

--- internal/pkg/shared/apm/apm_test.go
+++ internal/pkg/shared/apm/apm_test.go
@@ -50,13 +50,21 @@ func TestAPM(t *testing.T) {
 		t.Error("Expected result to be 'result test'")
 	}
 
-	defer tx.Recover()
-
 	// dont know how to verify the output of these without checking the output at apm server
 	tx.SetCustom("key", "val")
 	tx.SetError(errors.New("Test error"))
-
+	tx.Recover()
 	tx.End()
+	tx.Result("Try to change result")
+	if tx.Tx.Result == "Try to change result" {
+		t.Fatal("Expected to not be able to set result after End()")
+	}
+
+	// longtimeAgo := time.Now().AddDate(-30, 0, 0)
+	//	tx = StartTransaction("test", "test2", &longtimeAgo)
+	//	time.Sleep(time.Second)
+	tx.End()
+	defer tx.Recover()
 	trick := false // this is just a workaround to skip vet on reachable t.Error below
 	if !trick {
 		panic("panic")
