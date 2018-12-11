1d82464c57042287676a66bb2e911f42a4609a45
Cempakers87 <iman.nistelrooy@gmail.com>
2018-12-11 23:03:21 +0700

    test: edit elasticsearch unit test (#53)

--- web/ui/src/app/elasticsearch.service.spec.ts
+++ web/ui/src/app/elasticsearch.service.spec.ts
@@ -1,10 +1,18 @@
 import { TestBed } from '@angular/core/testing';
-
 import { ElasticsearchService } from './elasticsearch.service';
+import { HttpModule } from '@angular/http';
 
-describe('ElasticsearchService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
-
+describe('Elasticsearch Service', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+      ],
+      imports: [ 
+        HttpModule
+      ]
+    }).compileComponents();
+  });
+  
   it('should be created', () => {
     const service: ElasticsearchService = TestBed.get(ElasticsearchService);
     expect(service).toBeTruthy();
