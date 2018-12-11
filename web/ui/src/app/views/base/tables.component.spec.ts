839c247f088ab56a8c83bb1d167b9fe236acc5a6
Cempakers87 <iman.nistelrooy@gmail.com>
2018-12-11 23:10:14 +0700

    test: add initial alarm list test

---
+++ web/ui/src/app/views/base/tables.component.spec.ts
@@ -0,0 +1,43 @@
+import { TestBed, async } from '@angular/core/testing';
+import { TablesComponent } from './tables.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { NgxSpinnerModule, NgxSpinnerService } from 'ngx-spinner';
+import { ModalModule, AlertModule, ModalDirective } from 'ngx-bootstrap';
+import { MomentModule } from 'ngx-moment';
+import { HttpModule } from '@angular/http';
+import { ElasticsearchService } from '../../elasticsearch.service';
+
+describe('Alarm List Component', ()=>{
+
+  let fixture;
+  let app;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        TablesComponent
+      ],
+      imports: [ 
+        RouterTestingModule,
+        NgxSpinnerModule,
+        ModalModule.forRoot(),
+        AlertModule.forRoot(),
+        MomentModule,
+        HttpModule
+      ],
+      providers: [
+        NgxSpinnerService,
+        ElasticsearchService
+      ]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(TablesComponent);
+    app = fixture.debugElement.componentInstance;
+    
+  }));
+
+  it('should create the app', () => {
+    expect(app).toBeTruthy();
+  });
+
+});
