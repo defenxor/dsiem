02bdf4ff6d822b302a72b8de0a7185b6b31863c9
Cempakers87 <iman.nistelrooy@gmail.com>
2018-12-26 14:34:00 +0700

    test: add link to last page spec

--- web/ui/src/app/views/base/tables.component.spec.ts
+++ web/ui/src/app/views/base/tables.component.spec.ts
@@ -374,4 +374,10 @@ describe('Alarm List Component', ()=>{
     expect(next).toContain('Next');
   });
 
+  it('should have link to last page on pagination', ()=>{
+    fixture.detectChanges();
+    const last = fixture.nativeElement.querySelector('.pagination').textContent;
+    expect(last).toContain('Last');
+  });
+
 });
