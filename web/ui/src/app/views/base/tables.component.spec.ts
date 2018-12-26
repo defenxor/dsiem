25b7c1282538c0aa1d84b32e1c8be141d0a6e73d
Cempakers87 <iman.nistelrooy@gmail.com>
2018-12-26 14:31:45 +0700

    test: add link to previous page spec

--- web/ui/src/app/views/base/tables.component.spec.ts
+++ web/ui/src/app/views/base/tables.component.spec.ts
@@ -362,4 +362,10 @@ describe('Alarm List Component', ()=>{
     expect(first).toContain('First');
   });
 
+  it('should have link to previous page on pagination', ()=>{
+    fixture.detectChanges();
+    const prev = fixture.nativeElement.querySelector('.pagination').textContent;
+    expect(prev).toContain('Previous');
+  });
+
 });
