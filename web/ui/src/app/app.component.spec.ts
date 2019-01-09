508d48337eb1325ecd3e55152cdc0b70620c0276
Cempakers87 <iman.nistelrooy@gmail.com>
2019-01-09 07:29:10 +0700

    test: update app component spec

--- web/ui/src/app/app.component.spec.ts
+++ web/ui/src/app/app.component.spec.ts
@@ -2,8 +2,20 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { HttpModule } from "@angular/http";
+import { ElasticsearchService } from './elasticsearch.service';
+import { of } from 'rxjs';
+
 describe('App Component', () => {
+  let serviceStub;
+  let fixture;
+  let app;
+
   beforeEach(async(() => {
+    serviceStub = {
+      isAvailable: () => new Promise((resolve)=>{ resolve('')}),
+      getServer: () => of(),
+    }
+
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
@@ -11,13 +23,27 @@ describe('App Component', () => {
       imports: [ 
         RouterTestingModule,
         HttpModule
+      ],
+      providers: [
+        { provide: ElasticsearchService, useValue: serviceStub }
       ]
     }).compileComponents();
   }));
 
+  beforeEach(()=>{
+    fixture = TestBed.createComponent(AppComponent);
+    app = fixture.debugElement.componentInstance;
+  })
+
+  afterEach(()=>{
+    app.checkES = ()=>{
+      return null;
+    }
+  })
+
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
+    app.checkES();
     expect(app).toBeTruthy();
   });
+
 });
