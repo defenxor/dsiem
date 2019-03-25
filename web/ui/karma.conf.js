7efc3561fc2d42008579396b110303ba135f7c8e
Iman Wibowo <iman.nistelrooy@gmail.com>
2019-03-25 11:03:35 +0700

    ci: enable linting and testing (#71)
    * ci: enable linting and testing
    
    * test: edit karma config

--- web/ui/karma.conf.js
+++ web/ui/karma.conf.js
@@ -26,9 +26,9 @@ module.exports = function (config) {
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: true,
+    autoWatch: false,
     browsers: ['Chrome'],
-    singleRun: false,
+    singleRun: true,
     customLaunchers: {
       ChromeHeadlessCI: {
         base: 'ChromeHeadless',
