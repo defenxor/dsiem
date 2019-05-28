fcf395f63f15b1f76c2a264f9d7e8e9b6c711c43
mmta <memet.anwar@defenxor.com>
2019-05-29 05:02:56 +0700

    chore: add license header

--- web/ui/src/environments/environment.ts
+++ web/ui/src/environments/environment.ts
@@ -1,3 +1,20 @@
+/*
+Copyright (c) 2019 PT Defender Nusa Semesta and contributors, All rights reserved.
+
+This file is part of Dsiem.
+
+Dsiem is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation version 3 of the License.
+
+Dsiem is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Dsiem. If not, see <https:www.gnu.org/licenses/>.
+*/
 // The file contents for the current environment will overwrite these during build.
 // The build system defaults to the dev environment which uses `environment.ts`, but if you do
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
--- web/ui/src/polyfills.ts
+++ web/ui/src/polyfills.ts
@@ -1,3 +1,20 @@
+/*
+Copyright (c) 2019 PT Defender Nusa Semesta and contributors, All rights reserved.
+
+This file is part of Dsiem.
+
+Dsiem is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation version 3 of the License.
+
+Dsiem is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Dsiem. If not, see <https:www.gnu.org/licenses/>.
+*/
 /**
  * This file includes polyfills needed by Angular and is loaded before the app.
  * You can add your own extra polyfills to this file.
