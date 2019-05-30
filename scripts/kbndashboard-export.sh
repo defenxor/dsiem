27e4f6b1afb366f3a22dbd3abfd2baeb20dc3b83
mmta <mmta.gm@gmail.com>
2019-05-31 02:19:47 +0700

    chore: update dashboard scripts [skip-ci]

--- scripts/kbndashboard-export.sh
+++ scripts/kbndashboard-export.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+[ "$1" == "" ] || [ "$2" == "" ] && echo need the target kibana host as 1st argument, and dashboard json file output location as 2nd argument && exit 1
 
-curl -X GET "http://kibana.appl184.mss.defenxor.com:5601/api/kibana/dashboards/export?dashboard=87c18520-b337-11e8-b3e4-11404c6637fe" -H 'kbn-xsrf: true' > ./deployments/kibana/dashboard-siem.json
+host=$1
+echo downloading from kibana at http://${1}:5601 to $2 ..
+curl -s -f -X GET "http://${1}:5601/api/kibana/dashboards/export?dashboard=87c18520-b337-11e8-b3e4-11404c6637fe" -H 'kbn-xsrf: true' > $2 && \
+echo done.
 
