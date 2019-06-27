861420cb437be4c1f842825f7f9783017c245336
mmta <mmta@users.noreply.github.com>
2019-06-27 20:11:12 +0700

    add version info to web UI footer (#151)

---
+++ web/ui/version.ts
@@ -0,0 +1,22 @@
+const { gitDescribeSync } = require('git-describe');
+const { version } = require('./package.json');
+const { resolve, relative } = require('path');
+const { writeFileSync } = require('fs-extra');
+
+const gitInfo = gitDescribeSync({
+    dirtyMark: false,
+    dirtySemver: false
+});
+
+gitInfo.version = version;
+
+const file = resolve(__dirname, 'src', 'environments', 'version.ts');
+writeFileSync(file,
+`// IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
+/* tslint:disable */
+export const VERSION = ${JSON.stringify(gitInfo, null, 4)};
+/* tslint:enable */
+`, { encoding: 'utf-8' });
+
+console.log(`Wrote version info ${gitInfo.raw} to ${relative(resolve(__dirname, '..'), file)}`);
+
