fa669fc4645880d777d507e3d279470f412d6785
mmta1 <mmta@abydyl.net>
2019-10-18 23:12:37 +0700

    build: rm bash specific syntax

--- scripts/ngbuild-web.sh
+++ scripts/ngbuild-web.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
-command -v npm || { echo 'cannot find npm command in $PATH'; exit 1;}
+
+command -v npm >/dev/null || { echo 'cannot find npm command in $PATH'; exit 1; }
 
 dir="ui"
-[ "$1" == "internal" ] && dir="${dir}-internal"
+[ "$1" = "internal" ] && dir="${dir}-internal"
 cd web/$dir
 npm install
 npm run ng -- build --prod --build-optimizer --base-href /ui/
