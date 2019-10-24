defenxor/dsiem

commit ead2936a73a47ba14ef51b5a3b9d92d6501e1931

ci: release of linux ver. of the server binary (#208)
[skip ci]

scripts/gobuild-cmd-release.sh
@@ -24,24 +24,33 @@ for os in $goos; do
     n=$(basename $c)
     [ "$os" == "windows" ] && n="${n}.exe"
     echo building $c ver=${ver} buildtime=${now} for $os: $n ..
-    GOFLAGS="-mod=vendor" CGO_ENABLED=0 GOOS=$os GOARCH=amd64 go build -a -ldflags "-s -w -X main.version=${ver} -X main.buildTime=${now} -extldflags '-static'" -o $bdir/$n $c
+    GOFLAGS="-mod=vendor" CGO_ENABLED=0 GOOS=$os GOARCH=amd64 \
+    go build -a -ldflags "-s -w -X main.version=${ver} -X main.buildTime=${now} -extldflags '-static'" -o $bdir/$n $c || exit 1
   done
-  mkdir -p $bdir/web/dist && cp -r ./web/dist/* $bdir/web/dist/ || exit 1
-  cp -r ./configs ./LICENSE ./README.md $bdir/
-  cd $bdir
-  zname="$rdir/dsiem-server_${os}_amd64.zip"
-  echo "creating $zname .."
-  zip -9 -r $zname dsiem configs web LICENSE README.md
+
+  # release only the linux version of the server, there's no testing environment for Win/OSX version for this
+  # and we use drwmutex that only supports Linux
+  if [ "$os" = "linux" ]; then
+    mkdir -p $bdir/web/dist && cp -r ./web/dist/* $bdir/web/dist/ || exit 1
+    cp -r ./configs ./LICENSE ./README.md $bdir/
+    cd $bdir
+    zname="$rdir/dsiem-server_${os}_amd64.zip"
+    echo "creating $zname .."
+    dsiembin="dsiem"
+    [ "$os" == "windows" ] && dsiembin="${dsiembin}.exe"
+    zip -9 -r $zname $dsiembin configs web LICENSE README.md || exit 1
+  fi
+
   tools=$(ls | grep -v dsiem | grep -v configs | grep -v web)
   zname="$rdir/dsiem-tools_${os}_amd64.zip"
   echo "creating $zname .."
-  zip -9 $zname $tools
+  zip -9 $zname $tools || exit 1
   cd $curdir
   rm -rf $bdir
 done
 cd $rdir
 for f in $(ls $rdir | grep -v sha256); do
-  sha256sum $f > $f.sha256.txt
+  sha256sum $f > $f.sha256.txt || exit 1
 done
-echo "Done building, content of $dir:"
+echo "Done building, content of $rdir:"
 ls -l $rdir

commit fb2b42ed357020f66e74c6113049a5aefb96c18e

build: fix tools build err for win and darwin
the err caused by #208

[skip ci]

scripts/gobuild-cmd-release.sh
@@ -22,18 +22,20 @@ for os in $goos; do
   for c in $cmd; do
     [ ! -d $c ] && echo $c directory doesnt exist, skipping. && continue
     n=$(basename $c)
+    [ "$os" != "linux" ] && [ "$n" == "dsiem" ] && continue
     [ "$os" == "windows" ] && n="${n}.exe"
     echo building $c ver=${ver} buildtime=${now} for $os: $n ..
     GOFLAGS="-mod=vendor" CGO_ENABLED=0 GOOS=$os GOARCH=amd64 \
     go build -a -ldflags "-s -w -X main.version=${ver} -X main.buildTime=${now} -extldflags '-static'" -o $bdir/$n $c || exit 1
   done
 
+  mkdir -p $bdir/web/dist && cp -r ./web/dist/* $bdir/web/dist/ || exit 1
+  cp -r ./configs ./LICENSE ./README.md $bdir/
+  cd $bdir
+  
   # release only the linux version of the server, there's no testing environment for Win/OSX version for this
   # and we use drwmutex that only supports Linux
   if [ "$os" = "linux" ]; then
-    mkdir -p $bdir/web/dist && cp -r ./web/dist/* $bdir/web/dist/ || exit 1
-    cp -r ./configs ./LICENSE ./README.md $bdir/
-    cd $bdir
     zname="$rdir/dsiem-server_${os}_amd64.zip"
     echo "creating $zname .."
     dsiembin="dsiem"