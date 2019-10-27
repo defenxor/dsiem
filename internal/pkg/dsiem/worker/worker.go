defenxor/dsiem

commit 92d51ce14ee87347ff5ebae6c54fad699a1630fe

refactor: reinit msq queue on NATS error (#176)
* build: update NATS client and server libs

* refactor: reinit msg queue upon error

* refactor: switch back to use gnatsd alias

internal/pkg/dsiem/worker/worker.go
@@ -92,10 +92,6 @@ func GetBackPressureChannel() chan<- bool {
 }
 
 func initMsgQueue(msq string, prefix, nodeName string) (errOccurred bool) {
-	// TODO: maybe enable this if simply setting max retries doesn't work
-	// if transport != nil {
-	//	transport.Stop()
-	// }
 
 	initMsq := func() (err error) {
 		// reuse existing transport, used during testing
@@ -138,19 +134,16 @@ func Start(ch chan<- event.NormalizedEvent, msq string, msqPrefix string,
 		return err
 	}
 
-	_ = initMsgQueue(msq, msqPrefix, nodeName)
-
 	go func() {
+		_ = initMsgQueue(msq, msqPrefix, nodeName)
 		for {
-			evt := <-eventChan
-			ch <- evt
-		}
-	}()
-	go func() {
-		for err := range errChan {
-			log.Warn(log.M{Msg: "Error received from receive message queue: " + err.Error()})
-			// TODO: maybe enable this if simply setting max retries doesn't work
-			// _ = initMsgQueue(msq, msqPrefix, nodeName)
+			select {
+			case evt := <-eventChan:
+				ch <- evt
+			case err := <-errChan:
+				log.Warn(log.M{Msg: "Error received from receive message queue: " + err.Error()})
+				_ = initMsgQueue(msq, msqPrefix, nodeName)
+			}
 		}
 	}()
 

internal/pkg/dsiem/server/server_test.go
@@ -35,7 +35,7 @@ import (
 	"github.com/defenxor/dsiem/internal/pkg/shared/test"
 	"github.com/valyala/fasthttp"
 
-	gnatsd "github.com/nats-io/gnatsd/server"
+	gnatsd "github.com/nats-io/nats-server/v2/server"
 )
 
 // DefaultTestOptions are default options for the unit tests.

internal/pkg/dsiem/vice/nats/nats.go
@@ -21,12 +21,15 @@
 package nats
 
 import (
+	"errors"
+	"fmt"
 	"sync"
 
 	"github.com/defenxor/dsiem/internal/pkg/dsiem/event"
 	"github.com/defenxor/dsiem/internal/pkg/dsiem/vice"
 
-	"github.com/nats-io/go-nats"
+	log "github.com/defenxor/dsiem/internal/pkg/shared/logger"
+	"github.com/nats-io/nats.go"
 )
 
 // DefaultAddr is the NATS default TCP address.
@@ -98,10 +101,22 @@ func (t *Transport) newEncodedConnection() (*nats.EncodedConn, error) {
 		return t.natsEncodedConn, err
 	}
 	t.natsConn, err = nats.Connect(t.NatsAddr,
-		//nats.DisconnectHandler(func(nc *nats.Conn) {
-		//  handle disconnect event, maybe need to enable this later
-		//  t.errChan <- vice.Err{Name: "Disconnect", Err: errors.New("NATS server is disconnected")}
-		//}),
+		nats.DisconnectErrHandler(func(nc *nats.Conn, err error) {
+			msg := fmt.Sprintf("%q", err) // err maybe nil
+			log.Info(log.M{Msg: "Disconnected from NATS server. Reason: " + msg})
+		}),
+		nats.ReconnectHandler(func(nc *nats.Conn) {
+			log.Info(log.M{Msg: "Reconnected to NATS server"})
+		}),
+		nats.ErrorHandler(func(_ *nats.Conn, _ *nats.Subscription, err error) {
+			msg := fmt.Sprintf("%q", err)
+			log.Info(log.M{Msg: "NATS error occurred: " + msg})
+			t.errChan <- vice.Err{Name: "Error", Err: errors.New(msg)} // err maybe nil
+		}),
+		nats.ClosedHandler(func(nc *nats.Conn) {
+			msg := fmt.Sprintf("%q", nc.LastError()) // err maybe nil
+			log.Info(log.M{Msg: "NATS connection closed. Reason: " + msg})
+		}),
 		nats.MaxReconnects(-1))
 
 	if err == nil {

internal/pkg/dsiem/vice/nats/nats_test.go
@@ -25,7 +25,10 @@ import (
 	"time"
 
 	"github.com/defenxor/dsiem/internal/pkg/dsiem/event"
-	gnatsd "github.com/nats-io/gnatsd/server"
+	log "github.com/defenxor/dsiem/internal/pkg/shared/logger"
+	gnatsd "github.com/nats-io/nats-server/v2/server"
+
+	
 )
 
 // DefaultTestOptions are default options for the unit tests.
@@ -74,6 +77,8 @@ func stopNATS() {
 	}
 }
 func TestNATS(t *testing.T) {
+
+	log.Setup(true)
 
 	time.AfterFunc(time.Second*3, func() {
 		go RunDefaultServer()

internal/pkg/dsiem/vice/nats/options.go
@@ -16,7 +16,7 @@
 
 package nats
 
-import "github.com/nats-io/go-nats"
+import "github.com/nats-io/nats.go"
 
 // Options can be used to create a customized transport.
 type Options struct {

internal/pkg/dsiem/worker/worker_test.go
@@ -32,7 +32,7 @@ import (
 	"github.com/defenxor/dsiem/internal/pkg/dsiem/vice/nats"
 	"github.com/defenxor/dsiem/internal/pkg/shared/test"
 
-	gnatsd "github.com/nats-io/gnatsd/server"
+	gnatsd "github.com/nats-io/nats-server/v2/server"
 )
 
 // DefaultTestOptions are default options for the unit tests.
@@ -44,8 +44,6 @@ var DefaultTestOptions = gnatsd.Options{
 	MaxControlLine: 256,
 }
 
-// https://github.com/nats-io/gnatsd/blob/master/test/test.go
-// RunDefaultServer starts a new Go routine based server using the default options
 func RunDefaultServer() *gnatsd.Server {
 	return RunServer(&DefaultTestOptions)
 }

commit d1a09801d60029f22352af26f4315b0de4f9f511

fix: backpressure channel initialisation
error introduced in #176

[skip release]

internal/pkg/dsiem/worker/worker.go
@@ -38,12 +38,17 @@ var transport *nats.Transport
 var eventChan <-chan event.NormalizedEvent
 var bpChan chan<- bool
 var errChan <-chan error
+var bpChanReady chan struct{}
 
 type configFile struct {
 	Filename string `json:"filename"`
 }
 type configFiles struct {
 	Files []configFile `json:"files"`
+}
+
+func init() {
+	bpChanReady = make(chan struct{}, 1)
 }
 
 func getConfigFileList(frontendAddr string) (cf *configFiles, err error) {
@@ -88,6 +93,7 @@ func downloadConfigFiles(confDir string, frontendAddr string, node string) error
 
 //GetBackPressureChannel returns channel for sending backpressure bool messages
 func GetBackPressureChannel() chan<- bool {
+	<-bpChanReady
 	return bpChan
 }
 
@@ -112,6 +118,10 @@ func initMsgQueue(msq string, prefix, nodeName string) (errOccurred bool) {
 		err := initMsq()
 		if err == nil {
 			log.Info(log.M{Msg: "Successfully connected to message queue " + msq})
+			select {
+			case bpChanReady <- struct{}{}:
+			default:
+			}
 			break
 		}
 		errOccurred = true