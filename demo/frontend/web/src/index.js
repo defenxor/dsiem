e3d8a26a56adb87755a78490736e3efdc538780e
mmta <mmta.gm@gmail.com>
2019-11-09 20:06:05 +0700

    use named import for App

--- demo/frontend/web/src/index.js
+++ demo/frontend/web/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './components/app.jsx'
+import { App } from './components/app.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import './styles.css'
 
