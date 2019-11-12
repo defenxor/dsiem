067959ad313c294570318cd0294a739704d4fa92
mmta <mmta.gm@gmail.com>
2019-11-13 03:12:03 +0700

    add dpluger-reset.sh script to demo

---
+++ demo/scripts/dpluger-reset.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd $scriptdir
+
+git checkout ../dpluger/70_dsiem-plugin_suricata.conf
+rm -rf ../dpluger/directives_dsiem.json ../dpluger/*tsv ../docker/conf/dsiem/directives_dsiem.json
