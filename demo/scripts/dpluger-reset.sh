5deda096b62373b8511c14921cd600cbf661b0ec
mmta <mmta.gm@gmail.com>
2019-11-24 18:40:37 +0700

    chore: wrong directive file path in demo script
    [skip ci]

--- demo/scripts/dpluger-reset.sh
+++ demo/scripts/dpluger-reset.sh
@@ -4,4 +4,4 @@ scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $scriptdir
 
 git checkout ../dpluger/70_dsiem-plugin_suricata.conf
-rm -rf ../dpluger/directives_dsiem.json ../dpluger/*tsv ../docker/conf/dsiem/directives_dsiem.json
+rm -rf ../dpluger/directives_dsiem.json ../dpluger/*tsv ../docker/conf/dsiem/configs/directives_dsiem.json
