9a224bb10148f2292e8f3e496816b23620a7abac
rkspx <kosiriki@gmail.com>
2021-11-26 16:25:44 +0700

    add yes/no prompt method

--- internal/pkg/cmd/cmd.go
+++ internal/pkg/cmd/cmd.go
@@ -1,23 +1,57 @@
 package cmd
 
-import "log"
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"strings"
+)
 
 // Command is command-line utility, used for gathering input through prompt from user.
 type Command struct {
+	in  io.Reader
+	out io.Writer
 }
 
 // NewCommand creates a new Command and return its pointer.
-func NewCommand() *Command {
-	return &Command{}
+func NewCommand(in io.Reader, out io.Writer) *Command {
+	return &Command{in, out}
 }
 
 // PromptBool prompt a question with yes/no answer, returns true if user answered yes, returns false otherwise.
-func (c *Command) PromptBool(message string) bool {
-	// TODO: add implementation
-	panic("not implemented")
+func (c *Command) PromptBool(message string, def bool) bool {
+	var suffix string
+	if def {
+		suffix = "Y/n"
+	} else {
+		suffix = "y/N"
+	}
+
+	fmt.Fprintf(c.out, "%s (%s): ", message, suffix)
+	scanner := bufio.NewScanner(c.in)
+	for scanner.Scan() {
+		res := scanner.Text()
+		res = strings.TrimSpace(strings.ToLower(res))
+
+		if res == "" {
+			return def
+		}
+
+		if res == "y" || res == "yes" {
+			return true
+		}
+
+		if res == "n" || res == "no" {
+			return false
+		}
+
+		fmt.Fprintf(c.out, "please reply with 'y' or 'n': ")
+	}
+
+	return def
 }
 
 // Log log the message using Command default output.
 func (c *Command) Log(message string) {
-	log.Println(message)
+	fmt.Fprint(c.out, message)
 }
