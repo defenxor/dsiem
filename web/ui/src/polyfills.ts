295ca7f69793f882a82ac01bc719bada842f3015
rkspx <kosiriki@gmail.com>
2021-12-06 11:42:24 +0700

    upgrade angular to v12

--- web/ui/src/polyfills.ts
+++ web/ui/src/polyfills.ts
@@ -94,7 +94,7 @@ import 'mutationobserver-shim';
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
-import 'zone.js/dist/zone';  // Included with Angular CLI.
+import 'zone.js';  // Included with Angular CLI.
 
 
 
