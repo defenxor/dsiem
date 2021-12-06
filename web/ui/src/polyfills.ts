b754dcc5dde6075c75fe39974a09d3d5e5f4e5b4
rkspx <kosiriki@gmail.com>
2021-12-06 11:45:17 +0700

    upgrade angular to v13

--- web/ui/src/polyfills.ts
+++ web/ui/src/polyfills.ts
@@ -55,9 +55,6 @@ import 'core-js/es6/map';
 import 'core-js/es6/weak-map';
 import 'core-js/es6/set';
 
-/** IE10 and IE11 requires the following for NgClass support on SVG elements */
-// import 'classlist.js';  // Run `npm install --save classlist.js`.
-
 /** IE10 and IE11 requires the following for the Reflect API. */
 import 'core-js/es6/reflect';
 
@@ -68,12 +65,6 @@ import 'core-js/es7/reflect';
 
 // for IE10
 import 'mutationobserver-shim';
-
-/**
- * Required to support Web Animations `@angular/platform-browser/animations`.
- * Needed for: All but Chrome, Firefox and Opera. http://caniuse.com/#feat=web-animation
- **/
-// import 'web-animations-js';  // Run `npm install --save web-animations-js`.
 
 /**
  * By default, zone.js will patch all possible macroTask and DomEvents
