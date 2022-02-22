560115499e5f0c0be1a9cb58c53e30ca41d9b447
rkspx <kosiriki@gmail.com>
2022-02-22 20:19:16 +0700

    add option for including tsv-file

--- cmd/dpluger/run.go
+++ cmd/dpluger/run.go
@@ -17,6 +17,7 @@ func init() {
 	runCmd.Flags().BoolP("skipTLSVerify", "s", false, "whether to skip ES server certificate verification (when using HTTPS)")
 	runCmd.Flags().BoolP("usePipeline", "p", false, "whether to generate plugin that is suitable for logstash pipeline to pipeline configuration")
 	runCmd.Flags().BoolP("validate", "v", true, "Check whether each referred ES field exists on the target index")
+	runCmd.Flags().StringP("sid-list", "f", "", "optional, Plugin SID list file to use for generating logstash plugin (tsv-formatted). If set, dpluger will not generate any .tsv file, and assumes that you already have a .tsv file containing list of plugin SID, either by previous dpluger run or created manually")
 	viper.BindPFlag("validate", runCmd.Flags().Lookup("validate"))
 	viper.BindPFlag("skipTLSVerify", runCmd.Flags().Lookup("skipTLSVerify"))
 	viper.BindPFlag("usePipeline", runCmd.Flags().Lookup("usePipeline"))
@@ -33,6 +34,7 @@ var runCmd = &cobra.Command{
 		validate := viper.GetBool("validate")
 		skipTLSVerify := viper.GetBool("skipTLSVerify")
 		usePipeline := viper.GetBool("usePipeline")
+		SIDListFile := viper.GetString("sid-list")
 
 		if skipTLSVerify {
 			http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
@@ -44,13 +46,23 @@ var runCmd = &cobra.Command{
 		if err := log.Setup(true); err != nil {
 			exit("Cannot setup logger", err)
 		}
+
 		plugin, err := dpluger.Parse(config)
 		if err != nil {
 			exit("Cannot parse config file", err)
 		}
-		if err := dpluger.CreatePlugin(plugin, config, progName, validate, usePipeline); err != nil {
+
+		if err := dpluger.CreatePlugin(dpluger.CreatePluginConfig{
+			Plugin:      plugin,
+			ConfigFile:  config,
+			Creator:     progName,
+			Validate:    validate,
+			UsePipeline: usePipeline,
+			SIDListFile: SIDListFile,
+		}); err != nil {
 			exit("Error encountered while running config file", err)
 		}
+
 		fmt.Println("Logstash conf file created.")
 	},
 }
