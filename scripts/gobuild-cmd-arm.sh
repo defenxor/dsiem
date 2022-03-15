55cdef8822a6419d28839a3b9dc717c31c284ac7
rkspx <kosiriki@gmail.com>
2022-03-15 15:02:24 +0700

    add option to specify goos and goarch

--- scripts/gobuild-cmd-arm.sh
+++ scripts/gobuild-cmd-arm.sh
@@ -4,10 +4,12 @@ command -v go >/dev/null || { echo 'cannot find go command in $PATH'; exit 1; }
 
 cmd=${1}
 
-# cgoflag should be 0 or 1
-cgoflag=${2}
-xtraflag=${3}
-[ "$cgoflag" = "" ] && cgoflag=0
+goos=${2}
+[ -z $goos ] && goos=darwin
+
+goarch=${3}
+[ -z $goarch ] && goarch=arm64
+
 
 [ -z $cmd ] && cmd=$(find ./cmd/ -maxdepth 1 ! -path ./cmd/ -type d)
 
@@ -16,6 +18,6 @@ now=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 
 for c in $cmd; do
   [ ! -d $c ] && echo $c directory doesnt exist, skipping. && continue
-  echo building $c ver=${ver} buildtime=${now}
-  GOFLAGS="-mod=vendor" CGO_ENABLED=${cgoflag} GOOS=darwin GOARCH=arm64 go build ${xtraflag} -a -ldflags "-s -w -X main.version=${ver} -X main.buildTime=${now} -extldflags '-static'" $c
+  echo building $c ver=${ver} buildtime=${now} for $goos/$goarch
+  GOFLAGS="-mod=vendor" CGO_ENABLED=${cgoflag} GOOS=$goos GOARCH=$goarch go build ${xtraflag} -a -ldflags "-s -w -X main.version=${ver} -X main.buildTime=${now} -extldflags '-static'" $c
 done
