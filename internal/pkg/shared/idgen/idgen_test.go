649aa201960bd87b0a299248e6509df63d060546
rkspx <kosiriki@gmail.com>
2022-11-23 11:07:52 +0700

    add test for generating short-id

--- internal/pkg/shared/idgen/idgen_test.go
+++ internal/pkg/shared/idgen/idgen_test.go
@@ -17,12 +17,48 @@
 package idgen
 
 import (
+	"fmt"
+	"strings"
+	"sync"
 	"testing"
+
+	"golang.org/x/sync/errgroup"
 )
 
 func TestIdgen(t *testing.T) {
-	_, err := GenerateID()
-	if err != nil {
-		t.Fatal(err)
+	var lock sync.Mutex
+	m := map[string]bool{}
+	g := new(errgroup.Group)
+	for i := 0; i < 1000; i++ {
+		g.Go(func() error {
+			id, err := GenerateID()
+			if err != nil {
+				t.Error(err)
+				return err
+			}
+
+			if strings.HasPrefix(id, "-") {
+				return fmt.Errorf("id contain prefix '-', '%s'", id)
+			}
+
+			lock.Lock()
+			_, ok := m[id]
+			if ok {
+				return fmt.Errorf("id '%s' already generated", id)
+			}
+
+			m[id] = true
+			lock.Unlock()
+
+			return nil
+		})
+
 	}
+
+	if err := g.Wait(); err != nil {
+		t.Error(err)
+	}
+
+	t.Logf("generated %d unique ids", len(m))
+
 }
