d4a1245c135a27fd22fb7543df628eec610fe9c8
mmta <mmta.gm@gmail.com>
2024-02-06 17:43:30 +0700

    chore: add auth support

--- scripts/kbndashboard-export.sh
+++ scripts/kbndashboard-export.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 [ "$1" == "" ] || [ "$2" == "" ] && echo need the target kibana host as 1st argument, and dashboard json file output location as 2nd argument && exit 1
 
+CURL="curl -s -f"
+
+if [ ! -z "$ES_USERNAME" ] && [ ! -z "$ES_PASSWORD" ]; then
+  echo "** using ES_USERNAME and ES_PASSWORD env variable for authentication **"
+  CURL="curl -s -f -u $ES_USERNAME:$ES_PASSWORD"
+fi
+
 host=$1
 echo downloading from kibana at http://${1}:5601 to $2 ..
-curl -s -f -X GET "http://${1}:5601/api/kibana/dashboards/export?dashboard=87c18520-b337-11e8-b3e4-11404c6637fe" -H 'kbn-xsrf: true' > $2 && \
-echo done.
-
+curl -s -f -X GET "http://${1}:5601/api/kibana/dashboards/export?dashboard=87c18520-b337-11e8-b3e4-11404c6637fe" -H 'kbn-xsrf: true' >$2 &&
+  echo done.
--- scripts/kbndashboard-import.sh
+++ scripts/kbndashboard-import.sh
@@ -4,20 +4,27 @@
 
 host=$1
 dashboard=$(cat $2)
-command -v curl >/dev/null 2>&1 || { echo the required curl command is not available && exit 1 ;}
+command -v curl >/dev/null 2>&1 || { echo the required curl command is not available && exit 1; }
 
 while ! curl -fsS ${host}:5601 -o /dev/null; do
   echo cannot connect to ${host}:5601, will retry in 5 sec ..
   sleep 5
 done
 
-echo -n installing kibana dashboard to ${host}:5601 .. && \
-curl -fsS -o /dev/null -X POST "http://${host}:5601/api/kibana/dashboards/import?force=true" -H 'kbn-xsrf: true' -H 'Content-Type: application/json' -d "$dashboard"  && \
-echo done. && \
-echo -n setting default index to siem_alarms .. && \
-curl -fsS -o /dev/null -XPOST -H "Content-Type: application/json" -H "kbn-xsrf: true" ${host}:5601/api/kibana/settings/defaultIndex -d '{"value": "siem_alarms"}'  && \
-echo done && \
-echo dashboard installed successfully.
+CURL="curl -fsS -o /dev/null"
+
+if [ ! -z "$ES_USERNAME" ] && [ ! -z "$ES_PASSWORD" ]; then
+  echo "** using ES_USERNAME and ES_PASSWORD env variable for authentication **"
+  CURL="curl -fsS -o /dev/null -u $ES_USERNAME:$ES_PASSWORD"
+fi
+
+echo -n installing kibana dashboard to ${host}:5601 .. &&
+  $CURL -X POST "http://${host}:5601/api/kibana/dashboards/import?force=true" -H 'kbn-xsrf: true' -H 'Content-Type: application/json' -d "$dashboard" &&
+  echo done. &&
+  echo -n setting default index to siem_alarms .. &&
+  $CURL -XPOST -H "Content-Type: application/json" -H "kbn-xsrf: true" ${host}:5601/api/kibana/settings/defaultIndex -d '{"value": "siem_alarms"}' &&
+  echo done &&
+  echo dashboard installed successfully.
 
 # now for the extra siem_alarm_events idx pattern
 
@@ -26,5 +33,5 @@ patternfile="$(dirname $2)/idxpattern-siem_alarm_events.json"
 
 echo -n "Installing index pattern siem-alarm_events from $patternfile .. "
 idxpattern=$(cat $patternfile)
-curl -fsS -o /dev/null -X POST "http://${host}:5601/api/saved_objects/index-pattern/siem_alarm_events" -H 'kbn-xsrf: true' -H 'Content-Type: application/json' -d "$idxpattern" && \
-echo done || echo "failed to install, perhaps object already exist?"
+$CURL -X POST "http://${host}:5601/api/saved_objects/index-pattern/siem_alarm_events" -H 'kbn-xsrf: true' -H 'Content-Type: application/json' -d "$idxpattern" &&
+  echo done || echo "failed to install, perhaps object already exist?"
